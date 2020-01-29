f6a4b09cda8998cf52012832151ce9ca7f1da8c2
Azhan <43489763+azhanali@users.noreply.github.com>
2020-01-29 23:19:59 +0530

    The data is inserted at head.

---
+++ insertathead.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ 
+ class Node
+ {
+ 	public :
+ 		int data ;
+ 		Node * next;
+ 	
+ 	Node (int item)
+ 	{
+		data = item;
+ 		next = NULL;
+	 }
+ }; void push(Node* &head, int data)
+ {
+ 	Node* new_node = new Node (data);
+ 	new_node->next = (head);
+ 	head = new_node;
+ }
+ 
+ int main()
+ {
+ 	Node *head =NULL; 
+ 	int n;
+ 	cout <<"ENTER THE ELEMENTS: ";
+ 	cin >> n;
+ 	for(int i=0;i<n;i++)
+ 	{
+ 		int x;
+ 		cin >>x;
+ 		push(head,x);
+	 }
+ 	display(head);
+ return 0;
+ }
