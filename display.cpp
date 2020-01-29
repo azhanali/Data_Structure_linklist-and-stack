0b7b02140fa85da9f80f4bd444802edbad3d33b6
Azhan <43489763+azhanali@users.noreply.github.com>
2020-01-29 23:10:46 +0530

    Display the elements of the link list

---
+++ display.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ 
+ class Node
+ {
+ 	public :
+ 		int data ;
+ 		Node * next;
+ 	
+ 	Node (int item)
+ 	{
+		data = item;
+ 		next = NULL;
+	 }
+ };
+ 
+ void push(Node* &head, int data)
+ {
+ 	Node* new_node = new Node (data);
+ 	new_node->next = (head);
+ 	head = new_node;
+ }
+ void display(Node* &head)
+ {
+ 	Node* temp=head;
+ 	while (temp != NULL)
+ 	{
+ 		cout << temp->data << " ";
+ 		temp = temp->next;
+	 }
+ }
+ int main()
+ {
+ 	Node *head =NULL; 
+ 	int n;
+ 	cout <<"ENTER THE ELEMENTS: ";
+ 	cin >> n;
+ 	for(int i=0;i<n;i++)
+ 	{
+ 		int x;
+ 		cin >>x;
+ 		append(head,x);
+	 }
+ 	display(head);
+ 	return 0;
+ }
