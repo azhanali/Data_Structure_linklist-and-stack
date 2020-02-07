88e6809b21c5d3515cff691c198c9526708fb72f
Azhan <43489763+azhanali@users.noreply.github.com>
2020-02-07 23:01:12 +0530

    Created insertion at head at double link list.

---
+++ doubleLLInsertatahead.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ 
+ class Node
+ {
+ 	public :
+ 		int data ;
+ 		Node * next;
+ 		Node * prev;
+ 	
+ 	Node (int item)
+ 	{
+		data = item;
+ 		next = NULL;
+ 		prev = NULL;
+	 }
+ };
+ 
+ void insertathead(Node* &head, int data)
+ {
+ 	Node* new_node = new Node (data);
+ 	if(head!=NULL)
+ 	{   new_node->next=head;
+	 	head->prev = new_node->next;	
+	}
+ 	head = new_node;
+ }
+void display(Node* &head)
+ {
+ 	Node* temp=head;
+ 	while (temp != NULL)
+ 	{
+ 		cout <<temp->prev<<" " <<temp->data << " "<< temp->next<< " "<< temp<<endl;
+ 		temp = temp->next;
+	 }
+}int main()
+ {
+ 	Node *head =NULL; 
+ 	int n;
+ 	cout <<"ENTER THE ELEMENTS: ";
+ 	cin >> n;
+ 	for(int i=0;i<n;i++)
+ 	{
+ 		int x;
+ 		cin >>x;
+ 		insertathead(head,x);
+	}
+	cout<< "\n\n";
+ 	display(head);
+  return 0;
+  }
